0da5a74220337079402b591c0b79843a13acb22c
Alexandra <36158558+citrusonya@users.noreply.github.com>
2021-02-12 00:16:23 +0300

    update

---
+++ portfolio/js/script.js
@@ -0,0 +1,20 @@
+var mySwiper = new Swiper(".swiper-container", {
+  direction: "vertical",
+  loop: false,
+  pagination: ".swiper-pagination",
+  grabCursor: true,
+  speed: 1000,
+  paginationClickable: true,
+  parallax: true,
+  autoplay: false,
+  effect: "slide",
+  mousewheelControl: 1
+});
+
+$(function() {
+  $('nav [href]').each(function() {
+    if (this.href == window.location.href) {
+      $(this).addClass('active');
+    }
+  });
+});
