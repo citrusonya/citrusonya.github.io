f596e5a65a99399d74964a72ed6a50918fcbbb35
Alexandra <36158558+citrusonya@users.noreply.github.com>
2021-02-12 00:24:38 +0300

    Update script.js

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,17 +1,17 @@
-var mySwiper = new Swiper(".swiper-container", {
-  direction: "vertical",
-  loop: false,
-  pagination: ".swiper-pagination",
-  grabCursor: true,
-  speed: 1000,
-  paginationClickable: true,
-  parallax: true,
-  autoplay: false,
-  effect: "slide",
-  mousewheelControl: 1
-});
-
 $(function() {
+  var mySwiper = new Swiper(".swiper-container", {
+    direction: "vertical",
+    loop: false,
+    pagination: ".swiper-pagination",
+    grabCursor: true,
+    speed: 1000,
+    paginationClickable: true,
+    parallax: true,
+    autoplay: false,
+    effect: "slide",
+    mousewheelControl: 1
+  });
+  
   $('nav [href]').each(function() {
     if (this.href == window.location.href) {
       $(this).addClass('active');
