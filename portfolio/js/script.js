5b7408219938f7e015b3942de4d266f4b1ba41ab
Alexandra <36158558+citrusonya@users.noreply.github.com>
2021-02-12 20:34:57 +0300

    loader

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,4 +1,6 @@
+jQuery("body").prepend('<div id="loader"><div class="loader-item"></div><div class="loader-item"></div><div class="loader-item"></div></div>');
 $(function() {
+  jQuery("#loader").remove();
   $('nav [href]').each(function() {
     if (this.href == window.location.href) {
       $(this).addClass('active');
