7db07b4759b7ae148eba155d1ae423a2422a604c
Alexandra <36158558+citrusonya@users.noreply.github.com>
2021-02-12 17:50:54 +0300

    update

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,18 +1,4 @@
 $(function() {
-  var mySwiper = new Swiper(".swiper-container", {
-    direction: "vertical",
-    loop: false,
-    pagination: ".swiper-pagination",
-    grabCursor: true,
-    speed: 1000,
-    paginationClickable: true,
-    parallax: true,
-    autoplay: false,
-    effect: "slide",
-    mousewheelControl: 1
-  });
-
-
   $('nav [href]').each(function() {
     if (this.href == window.location.href) {
       $(this).addClass('active');
